80ebf974e9264682dd5bf6b3ed8b552b2858aae3
navyas211@gmail.com <navyas211@gmail.com>
2021-09-29 15:56:48 +0530

    [NAVYA]ADDED: Welcome to Line Comparison Problems

---
+++ src/com/bridgelabz/LineComparisonProblem/WelcomeLineComparisonProgram.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.LineComparisonProblem;
+
+public class WelcomeLineComparisonProgram {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison Computation Program ");
+	}
+}
